255dcb5b17923981acbbdc46a5b3c18b17ac8595
pedromakaveli <pedrolacerda804@gmail.com>
2025-03-19 20:45:44 +0000

    init repo

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('produtos/', include('produtos.urls'))
+    path('', views.index, name="index"),
+    path('criar_produto', views.criar_produto, name="criar_produto")
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index (request):
+    return HttpResponse("Página de produtos")
+
+def criar_produto (request):
+    return HttpResponse("Página para criar produto")
