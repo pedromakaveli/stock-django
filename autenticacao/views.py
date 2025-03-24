4d8550942937fe3f9cf3912f785790d64bdac26c
pedromakaveli <pedrolacerda804@gmail.com>
2025-03-24 23:01:27 +0000

    Adiciona criação de usuários e login

---
+++ autenticacao/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from produtos import urls
+# Create your views here.
+
+def login (request):
+    if request.method == "GET":
+        return render(request, "login.html")
+    
+    if request.method == "POST":
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        
+        user = authenticate(request, username = email, password = senha)
+        
+        print(email, senha)
+        if user is not None:
+
+            return redirect("index")
+        else:
+            messages.error(request, "Credenciais inválidas ou usuário não existe.")
+            return redirect("login")  # Redireciona de volta ao login
+        
+
+def cadastro (request):
+    
+    if request.method == "GET":
+        return render(request, "cadastro.html")
+
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        sobrenome = request.POST.get("sobrenome")
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        
+        print(nome,sobrenome,email,senha)
+        
+        user = User.objects.create_user(first_name=nome, username=email, password=senha)
+        user.last_name = sobrenome
+        user.save()
+        
+        return render(request, "cadastro.html")
